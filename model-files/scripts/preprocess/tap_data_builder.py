f415ddb288148901585924d91bf6a2f25928d5b5
Lisa Zorn <lzorn@mtc.ca.gov>
2015-02-10 12:06:17 -0800

    Use pandas and use pandas for parsing, writing, grouping rather than doing it ourselves, to make this more readable and maintainable.

--- model-files/scripts/preprocess/tap_data_builder.py
+++ model-files/scripts/preprocess/tap_data_builder.py
@@ -1,85 +1,84 @@
 """
-    tap_data_builder.py model_run_dir 
+    tap_data_builder.py base_dir 
 
-    inputs: model_run_dir - the directory the model is running in (directory with INPUTS, hwy, etc.)
+    inputs: base_dir - the directory the model is running in (directory with INPUTS, hwy, etc.)
 
-    This script builds the tap data file. This file is a csv data file which contains data about taps
-    and is used by the CTRAMP model. It consists of the following columns:
+    This script builds the tap csv data file which maps all TAPs to the closest TAZ for that TAP.
+
+    Input:
     
-        tap - the tap number (in CTRAMP sequential numbering)
-        tap_original - the original tap number (in the CUBE network)
+        hwy\mtc_final_network_zone_seq.csv - the file holding the correspondence between network zone numbers and
+                                             CTRAMP sequential numbering
+        hwy\tap_to_taz_for_parking.txt - the file holding the walk distance shortest path tree from taps to tazs
+        
+    Output: hwy\tap_data.csv with the following columns
+
+        TAP - the tap number (in CTRAMP sequential numbering)
+        TAP_original - the original tap number (in the CUBE network)
         lotid - the lot id; this is the same as tap
-        taz - the taz the tap is associated with (see tap_to_taz_for_parking.job)
+        TAP - the taz the tap is associated with (see tap_to_taz_for_parking.job)
         capacity - the capacity of the lot; this is set to 9999 by default, but could be changed after 
                    this process has run
-                   
-    This script uses the following files:
-    
-        mtc_final_netwok_zone_seq.csv - the file holding the correspondence between network zone numbers and
-                                        CTRAMP sequential numbering
-        tap_to_taz_for_parking.txt - the file holding the walk distance shortest path tree from taps to tazs
-        
-    Output: tap_data.csv (as described above)
     
     crf 11/2013
 
 """
 
 import os,sys
+import pandas
 
-model_run_dir = sys.argv[1]
-zone_mappng_file = os.path.join(model_run_dir,r'hwy\mtc_final_network_zone_seq.csv')
-infile = os.path.join(model_run_dir,r'hwy\tap_to_taz_for_parking.txt')
-outfile = os.path.join(model_run_dir,r'hwy\tap_data.csv')
+if __name__ == '__main__':
+    base_dir                = sys.argv[1]
+    zone_seq_mapping_file   = os.path.join(base_dir,'hwy',      'mtc_final_network_zone_seq.csv')
+    infile                  = os.path.join(base_dir,'hwy',      'tap_to_taz_for_parking.txt')
+    outfile                 = os.path.join(base_dir,'hwy',      'tap_data.csv')
 
-#read the sequence mapping and build mappings and a list of all taps
-seq_mapping = {}
-taps = []
-tap_mapping = {}
-for line in open(zone_mappng_file):
-    data = map(int,line.strip().split(','))
-    if data[1] > 0:
-        seq_mapping[data[0]] = str(data[1])
-    if data[2] > 0:
-        seq_mapping[data[0]] = str(data[2])
-    if data[3] > 0:
-        seq_mapping[data[0]] = str(data[3])
-        taps.append(data[0])
-    if data[4] > 0:
-        seq_mapping[data[0]] = str(data[4])
+    sequence_mapping        = pandas.DataFrame.from_csv(zone_seq_mapping_file)
+    sequence_mapping.reset_index(inplace=True)
 
-#create the data for the file, using the tap to taz file as the structure for the loop
-# only the taz that is closest to a given tap is retained
-data = {}
-for line in open(infile):
-    line = line.strip()
-    if len(line) == 0:
-        continue
-    line = line.split(',')
-    tap = int(line[0])
-    taz = int(line[1])
-    distance = float(line[4])
-    if (not tap in data) or (distance < data[tap][0]):
-        data[tap] = (distance,[seq_mapping[tap],tap,seq_mapping[tap],seq_mapping[taz],9999])
+    tap_data                = pandas.read_table(infile, names=['TAP_original','TAZ_original','TAZ2','SP_DISTANCE','FEET'],
+                                                delimiter=',')
+    tap_data_grouped        = tap_data.groupby('TAP_original')
 
-#write the output file
-f = open(outfile,'wb')
-f.write(','.join(['tap','tap_original','lotid','taz','capacity']) + os.linesep)
-taps.sort()
-for tap in taps:
-    tappy = tap
-    #if it isn't in our list, then just pick the next lowest tap - not the greatest, but ok
-    # this occurs because the shortest path for tap->taz is limited to a few miles
-    # this should not happen, if it is reported, then the threshold for cutting off tap->taz
-    # skims should be increased
-    if not tappy in data:
-        print 'tap (' + str(tappy) + ')not captured in tap->taz (for parking) script; max distance for that should be increased'
-    while not tappy in data: 
-        tappy -= 1
-    d = list(data[tappy][1])
-    if tap != tappy:
-        d[0] = seq_mapping[tap]
-        d[1] = tap
-        d[2] = seq_mapping[tap]
-    f.write(','.join(map(str,d)) + os.linesep)
-f.close()
+    tap_data_out_init       = False
+    tap_data_out            = None
+    taps                    = sequence_mapping[sequence_mapping.TAPSEQ > 0]
+    for ind,row in taps.iterrows():
+        try:
+            sorted_group = tap_data_grouped.get_group(row['N']).sort(['FEET','TAZ_original'])
+            if tap_data_out_init:
+                tap_data_out = tap_data_out.append(sorted_group.head(1))
+            else:
+                tap_data_out = sorted_group.head(1)
+                tap_data_out_init = True
+
+        except KeyError:
+            print 'tap %8d not captured in tap->taz (for parking) script' % row['N']
+            # use the last one -- does this make sense?
+            use_this = tap_data_out.tail(1).copy()
+            use_this.loc[:,'TAP_original'] = row['N']
+            tap_data_out = tap_data_out.append(use_this)
+
+    tap_data_out.reset_index(inplace=True)
+
+    # join to get the real TAZ
+    tap_data_out = pandas.merge(left=sequence_mapping[['N','TAZSEQ']], right=tap_data_out, how='right',
+                                left_on='N', right_on='TAZ_original')    # rename sequence column
+    tap_data_out.rename(columns={'TAZSEQ':'TAZ', 'N':'dropN1'}, inplace=True)
+
+    # join to get the real TAP
+    tap_data_out = pandas.merge(left=sequence_mapping[['N','TAPSEQ']], right=tap_data_out, how='right',
+                                left_on='N', right_on='TAP_original')
+    tap_data_out.rename(columns={'TAPSEQ':'TAP', 'N':'dropN2'}, inplace=True)
+
+    # drop others
+    tap_data_out.drop(['index','FEET','SP_DISTANCE','TAZ_original','TAZ2','dropN1','dropN2'], axis=1, inplace=True)
+
+    # are these really useful??
+    tap_data_out['lotid']       = tap_data_out['TAP']
+    tap_data_out['capacity']    = 9999
+
+    # reorder and write
+    tap_data_out = tap_data_out[['TAP','TAP_original','lotid','TAZ','capacity']]
+    tap_data_out.to_csv(outfile, index=False)
+    print "Wrote %s" % outfile
