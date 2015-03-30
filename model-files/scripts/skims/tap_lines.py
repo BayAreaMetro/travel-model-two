419a4fb4591bbfdc8d61d7a5c3bcdc29d67bb863
Lisa Zorn <lzorn@mtc.ca.gov>
2015-03-30 14:17:26 -0700

    Trivial changes that don't change results (use csv header, accurate runtime report, etc)

--- model-files/scripts/skims/tap_lines.py
+++ model-files/scripts/skims/tap_lines.py
@@ -5,24 +5,21 @@
   Ben Stabler, stabler@pbworld.com, 12/23/13
 """
 
-import os,sys,re, csv
-import time as pytime
+import csv,os,re,sys,time
 
 ################################################################################
-
-base_dir = os.getcwd()
 
 #input files
-transit_line_file = os.path.join(base_dir,r'trn\transitLines.lin')
-network_tap_links_file = os.path.join(base_dir,r'hwy\mtc_final_network_tap_links.csv')
-zone_seq_file = os.path.join(base_dir,r'hwy\mtc_final_network_zone_seq.csv')
+transit_line_file       = os.path.join('trn',   'transitLines.lin')
+network_tap_links_file  = os.path.join('hwy',   'mtc_final_network_tap_links.csv')
+zone_seq_file           = os.path.join('hwy',   'mtc_final_network_zone_seq.csv')
 
 #output file
-tap_lines_file = os.path.join(base_dir,r'trn\tapLines.csv')
+tap_lines_file          = os.path.join('trn',   'tapLines.csv')
 
 ################################################################################
 
-start_time = pytime.time()
+start_time = time.time()
 
 print 'reading transit lines'
 linesByNode = dict()
@@ -40,35 +37,25 @@ for line in open(transit_line_file):
       linesByNode[n].add(lineName.replace('"',""))
 
 print 'reading tap connectors'
-access_links = []
+nodesByTap = dict()
 with open(network_tap_links_file, 'rb') as csvfile:
   tapreader = csv.reader(csvfile, skipinitialspace=True)
   for row in tapreader:
-    access_links.append(row)
+    tap = int(row[0])
+    node = int(row[1])
+    if tap not in nodesByTap:
+      nodesByTap[tap] = set()
+    nodesByTap[tap].add(node)
 
 print 'reading zone sequence file'
 tapToSeqTap = dict()
-first = True
 with open(zone_seq_file, 'rb') as csvfile:
-  tapreader = csv.reader(csvfile, skipinitialspace=True)
+  tapreader = csv.DictReader(csvfile, skipinitialspace=True)
   for row in tapreader:
-    if first:
-      first = False
-      continue
-    node_id = int(row[0])
-    seq_tap_id = int(row[3])
+    node_id    = int(row['N'])
+    seq_tap_id = int(row['TAPSEQ'])
     if seq_tap_id > 0:
       tapToSeqTap[node_id] = seq_tap_id
-
-#get nodes connected to each tap
-nodesByTap = dict()
-for linkRow in access_links:
-  tap = int(linkRow[0])
-  node = int(linkRow[1])
-  
-  if tap not in nodesByTap:
-    nodesByTap[tap] = set()
-  nodesByTap[tap].add(node)
 
 #get lines for each tap
 linesByTap = dict()
@@ -92,5 +79,5 @@ for tap in linesByTap.keys():
     f.write("%s,%s\n" % (tapToSeqTap[tap],lines))
 f.close()
 
-end_time = pytime.time()
-print 'elapsed time in seconds: ' + str((end_time - start_time) / 1000.0)
+end_time = time.time()
+print 'elapsed time in minutes: ' + str((end_time - start_time) / 60.0)
