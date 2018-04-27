a25183ceb4c46c842ba11fdd86ba2cf1f603fd0d
Lisa Zorn <lzorn@bayareametro.gov>
2018-04-27 09:42:54 -0700

    More updates to the maz/taz checker

--- utilities/maz_taz_checker.py
+++ utilities/maz_taz_checker.py
@@ -2,13 +2,24 @@ USAGE = """
 
 MAZ and TAZ checker.
 
+Reads the definition of MAZs and TAZs in blocks_mazs_tazs.dbf (converted from csv using csv_to_dbf.R)
+
+The following verifcations are performed
   - Verifies that all blocks with nonzero land area are assigned a maz/taz
-  - Verifies(*) that there are not multiple tazs, tracts or counties per maz
-  - TODO: add block group?
+  - Verifies that there are not multiple tazs or counties per maz
+  - Counts/lists first 30 mazs with multiple block groups, tracts
+  - Verifies that there are not multiple counties per taz
+
+Creates a dissolved maz shapefile and a dissolved taz shapefile.
 
   Notes:
-  - Block "060750179021009" (maz 315303, taz 300206) is the little piece of Alameda island that the Census 2010 
-    calls San Francisco.  We put it in Alameda.
+  - Block "06 075 017902 1009" (maz 10186, taz 592) is the little piece of Alameda island that the Census 2010
+    calls San Francisco.  Left in SF as its own maz.
+
+  - Blocks "06 075 980401 100[1,2,3]" (maz 16084, taz 287) are the Farallon Islands.  It's a standalone maz.
+
+  - Blocks "06 075 017902 10[05,80]" (maz 16495, taz 312) is a tiny sliver that's barely land so not worth
+    making a new maz, so that maz includes a second tract (mostly water)
 
   TODO: remove dependency on dbfread?  Since we're using arcpy, can just use that to read the dbf
 
@@ -24,7 +35,6 @@ import arcpy
 
 WORKSPACE          = "M:\\Data\\GIS layers\\TM2_maz_taz_v2.1"
 CROSSWALK_ROOT     = "blocks_mazs_tazs"
-CROSSWALK_CSV      = os.path.join(WORKSPACE, "{0}.csv".format(CROSSWALK_ROOT))
 CROSSWALK_DBF      = os.path.join(WORKSPACE, "{0}.dbf".format(CROSSWALK_ROOT))
 
 CENSUS_BLOCK_DIR   = "M:\\Data\\Census\\Geography\\tl_2010_06_tabblock10"
@@ -89,20 +99,38 @@ if __name__ == '__main__':
     # if maz is not zero, taz should not be zero
     assert(blocks_maz_df.taz != 0).all()
 
-    # verify one taz/TRACT/COUNTY per unique maz
-    # add block group?
-    for bigger_geo in ["taz","GEOID10_TRACT","GEOID10_COUNTY"]:
+    # verify one taz/BLOCK GROUP/TRACT/COUNTY per unique maz
+    # error for taz/COUNTY
+    # warn/log for BLOCK GROUP/TRACT
+    for bigger_geo in ["taz","GEOID10_BG","GEOID10_TRACT","GEOID10_COUNTY"]:
         maz_geo_df = blocks_maz_df[["maz",bigger_geo]].groupby(["maz"]).agg("nunique")
         maz_multiple_geo_df = maz_geo_df.loc[ maz_geo_df[bigger_geo] > 1]
         if len(maz_multiple_geo_df) == 0:
             print("Verified one {0} per maz".format(bigger_geo))
         else:
-            error = "ERROR: Multiple {0} for a single maz:".format(bigger_geo)
-            print(error)
-            print(maz_multiple_geo_df)
-            print("")
-            # sys.exit(error)
+            if bigger_geo in ["GEOID10_BG","GEOID10_TRACT"]:
+                fatal = False
+            else:
+                fatal = True
 
+            error = "{0}: Multiple {1} for a single maz: {2}".format(
+                    "ERROR" if fatal else "WARNING", bigger_geo, len(maz_multiple_geo_df))
+            print(error)
+            print(maz_multiple_geo_df.head(30))
+            print("")
+            if fatal: sys.exit(error)
+
+    # verify one COUNTY per unique taz
+    taz_county_df = blocks_maz_df[["taz","GEOID10_COUNTY"]].groupby(["taz"]).agg("nunique")
+    taz_multiple_county_df = taz_county_df.loc[ taz_county_df["GEOID10_COUNTY"] > 1]
+    if len(taz_multiple_county_df) == 0:
+        print("Verified one county per taz")
+    else:
+        error = "ERROR: Multiple counties for a single taz: {1}".format(len(taz_multiple_county_df))
+        print(error)
+        print(taz_multiple_county_df)
+        print("")
+        sys.exit(error)  # this is required
 
     # count blocks per maz
     count_df = blocks_maz_df[["GEOID10","maz"]].groupby(["maz"]).agg("nunique")
@@ -172,6 +200,13 @@ if __name__ == '__main__':
                                    "MULTI_PART", "DISSOLVE_LINES")
         print("Dissolved mazs into {0}.shp".format(MAZS_SHP))
 
+        # add maz mod 1000 to make it easier to add symbology
+        arcpy.AddField_management("{0}.shp".format(MAZS_SHP), "maz_mod_1k", "SHORT")
+        print("Added maz_mod_1k field")
+        arcpy.CalculateField_management("{0}.shp".format(MAZS_SHP), "maz_mod_1k",
+                                        "!maz! % 1000", "PYTHON3")
+        print("Calculated maz_mod_1k field")
+
         # create tazs shapefile
         arcpy.Dissolve_management (layer_name, TAZS_SHP, "{0}.taz".format(CROSSWALK_ROOT),
                                    [["{0}.ALAND10".format(CENSUS_BLOCK_ROOT),  "SUM"  ],
@@ -179,7 +214,14 @@ if __name__ == '__main__':
                                     ["{0}.GEOID10".format(CENSUS_BLOCK_ROOT),  "COUNT"],  # count block per taz
                                     ["{0}.maz".format(CROSSWALK_ROOT),         "COUNT"]], # count maz per taz
                                    "MULTI_PART", "DISSOLVE_LINES")
-        print("Dissolved mazs into {0}.shp".format(MAZS_SHP))
+        print("Dissolved tazs into {0}.shp".format(TAZS_SHP))
+
+        # add maz mod 1000 to make it easier to add symbology
+        arcpy.AddField_management("{0}.shp".format(TAZS_SHP), "taz_mod_1k", "SHORT")
+        print("Added taz_mod_1k field")
+        arcpy.CalculateField_management("{0}.shp".format(TAZS_SHP), "taz_mod_1k",
+                                        "!taz! % 1000", "PYTHON3")
+        print("Calculated taz_mod_1k field")
 
     except Exception as err:
         print(err.args[0])
