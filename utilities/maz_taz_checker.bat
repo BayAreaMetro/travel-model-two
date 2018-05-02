dc59a479ff43854cc98f99e21e95bf4c12b6c1dc
Lisa Zorn <lzorn@bayareametro.gov>
2018-05-01 19:23:52 -0700

    Add loop to batch file

--- utilities/maz_taz_checker.bat
+++ utilities/maz_taz_checker.bat
@@ -2,27 +2,36 @@
 :: batch file to run maz_taz_checker interatively in a semi-automated way
 ::
 
+setlocal enabledelayedexpansion
+
 set WORKSPACE=M:\Data\GIS layers\TM2_maz_taz_v2.2
 set TM2_DIR=C:\Users\lzorn\Documents\travel-model-two
-set VERSION_CURR=2.1.1
-set VERSION_NEXT=2.1.2
 
-copy blocks_mazs_tazs_v%VERSION_CURR%.csv blocks_mazs_tazs.csv
+:: loop
+for /L %%V in (1,1,4) do (
 
-call "C:\Program Files\R\R-3.4.1\bin\x64\Rscript.exe" --vanilla "%WORKSPACE%\csv_to_dbf.R"
-IF ERRORLEVEL 1 goto error
+  set /a NEXTV=%%V+1
+  echo 2.1.%%V 2.1.!NEXTV!
 
-:: save the dbf for this version
-copy blocks_mazs_tazs.dbf blocks_mazs_tazs_v%VERSION_CURR%.dbf
+  copy blocks_mazs_tazs_v2.1.%%V.csv blocks_mazs_tazs.csv
 
-:: clear the dissolve shapefiles so they'll regenerate
-del mazs_TM2_v2_2.*
-del tazs_TM2_v2_2.*
-python "%TM2_DIR%\utilities\maz_taz_checker.py"
-IF ERRORLEVEL 1 goto error
+  call "C:\Program Files\R\R-3.4.1\bin\x64\Rscript.exe" --vanilla "%WORKSPACE%\csv_to_dbf.R"
+  IF ERRORLEVEL 1 goto error
 
-copy maz_taz_checker.log maz_taz_checker_v%VERSION_NEXT%.log
-copy blocks_mazs_tazs_updated.csv blocks_mazs_tazs_v%VERSION_NEXT%.csv
+  rem save the dbf for this version
+  copy blocks_mazs_tazs.dbf blocks_mazs_tazs_v2.1.%%V.dbf
+
+  rem clear the dissolve shapefiles so they'll regenerate
+  del mazs_TM2_v2_2.*
+  del tazs_TM2_v2_2.*
+
+  if not %%V==4 python "%TM2_DIR%\utilities\maz_taz_checker.py"
+  if %%V==4 python "%TM2_DIR%\utilities\maz_taz_checker.py" --dissolve
+  IF ERRORLEVEL 1 goto error
+
+  copy maz_taz_checker.log maz_taz_checker_v2.1.!NEXTV!.log
+  copy blocks_mazs_tazs_updated.csv blocks_mazs_tazs_v2.1.!NEXTV!.csv
+)
 
 echo Success!
 goto done
