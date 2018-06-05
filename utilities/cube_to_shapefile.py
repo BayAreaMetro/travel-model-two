bd6feefe2be3c1b601b5171a28f9e0dcf29b5d3a
Lisa Zorn <lzorn@bayareametro.gov>
2018-06-05 10:11:09 -0700

    Update export network
    Played with transit but no dice.  But let cube do most of the roadway bit.

--- utilities/cube_to_shapefile.py
+++ utilities/cube_to_shapefile.py
@@ -10,15 +10,7 @@ import argparse, csv, logging, os, subprocess, sys, traceback
 import numpy, pandas
 
 RUNTPP_PATH     = "C:\\Program Files (x86)\\Citilabs\\CubeVoyager"
-PROJ_FILE       = "NAD 1983 StatePlane California VI FIPS 0406 (US Feet).prj"
 LOG_FILE        = "cube_to_shapefile.log"
-
-# temporary files in the CWD
-NODE_DBFFILE    = "nodes.dbf"
-LINK_DBFFILE    = "links.dbf"
-LINK_POINTS_DBF = "link_points_table.dbf"
-LINK_NOATTRS    = "link_noattr"
-LINK_NOATTRS_SHP= "{0}.shp".format(LINK_NOATTRS)
 
 # shapefiles
 NODE_SHPFILE    = "network_nodes.shp"
@@ -93,142 +85,27 @@ if __name__ == '__main__':
     logger.addHandler(fh)
 
     parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.RawDescriptionHelpFormatter,)
-    parser.add_argument("netfile", metavar="network.net", nargs=1, help="Cube input network file")
+    parser.add_argument("netfile", metavar="network.net", nargs=1, help="Cube input roadway network file")
+    parser.add_argument("linefile", metavar="transit.lin", nargs=1, help="Cube input transit line file")
     args = parser.parse_args()
 
     # setup the environment
     script_env                 = os.environ.copy()
     script_env["PATH"]         = "{0};{1}".format(script_env["PATH"],RUNTPP_PATH)
+    script_env["LINE_INFILE"]  = args.linefile[0]
     script_env["NET_INFILE"]   = args.netfile[0]
-    script_env["NODE_OUTFILE"] = NODE_DBFFILE
-    script_env["LINK_OUTFILE"] = LINK_DBFFILE
+    script_env["NODE_OUTFILE"] = NODE_SHPFILE
+    script_env["LINK_OUTFILE"] = LINK_SHPFILE
 
-    # run the script to create NODE_DBFFILE and LINK_DBFFILE
+    # run the script to do the work
     runCubeScript(WORKING_DIR, os.path.join(CODE_DIR, "export_network.job"), script_env)
 
     # MakeXYEventLayer_management
     import arcpy
     arcpy.env.workspace = WORKING_DIR
 
-    # don't care if this fails, just want to head off error since arcpy gets mad if we try to overwrite
-    try:
-        arcpy.Delete_management(NODE_SHPFILE)
-    except Exception as err:
-        logging.debug(err.args[0])
+    # define the spatial reference
+    sr = arcpy.SpatialReference(102646)
+    arcpy.DefineProjection_management(NODE_SHPFILE, sr)
+    arcpy.DefineProjection_management(LINK_SHPFILE, sr)
 
-    try:
-        arcpy.Delete_management(LINK_POINTS_DBF)
-    except Exception as err:
-        logging.debug(err.args[0])
-
-    try:
-        arcpy.Delete_management(LINK_NOATTRS_SHP)
-    except Exception as err:
-        logging.debug(err.args[0])
-
-    try:
-        arcpy.Delete_management(LINK_SHPFILE)
-    except Exception as err:
-        logging.debug(err.args[0])
-
-    try:
-        node_layer = "node_layer"
-        arcpy.MakeXYEventLayer_management(NODE_DBFFILE, in_x_field="X", in_y_field="Y",
-                                          out_layer=node_layer, spatial_reference=os.path.join(WORKING_DIR, PROJ_FILE))
-        arcpy.CopyFeatures_management(node_layer, NODE_SHPFILE)
-        logging.info("Created {0}".format(NODE_SHPFILE))
-
-        # create Dataframe
-        node_field_names = [
-            "N","X","Y","COUNTY","MODE","STOP","PNR_CAP",
-            "PNR1","PNR2","PNR3","PNR4","PNR5",
-            "PNR_FEE1","PNR_FEE2","PNR_FEE3","PNR_FEE4","PNR_FEE5", "FAREZONE"
-        ]
-        nodes_df = pandas.DataFrame(arcpy.da.FeatureClassToNumPyArray(in_table=node_layer, field_names=node_field_names))
-        logging.info("nodes_df has length {0}".format(len(nodes_df)))
-        logging.info(nodes_df.head())
-
-        # for links, first we make a points layer with A and B points
-        link_table_layer = "link_table_layer"
-        arcpy.MakeTableView_management(LINK_DBFFILE, link_table_layer)
-        if len(arcpy.ListFields(link_table_layer, "A_B"))==0:
-            arcpy.AddField_management(link_table_layer, "A_B", "TEXT", 21)
-            arcpy.CalculateField_management(link_table_layer, "A_B", "str(!A!)+'_'+str(!B!)", "PYTHON3")
-            logging.info("Calculated A_B")
-
-        links_df = pandas.DataFrame(arcpy.da.FeatureClassToNumPyArray(in_table=link_table_layer, field_names=["A","B","A_B"]))
-        logging.debug("links_df has length {0}".format(len(links_df)))
-        logging.debug("\n{0}".format(links_df.head()))
-
-        link_A_points_df = pandas.merge(left=links_df[["A_B","A"]], right=nodes_df[["N","X","Y"]], left_on="A", right_on="N", how="left")
-        link_B_points_df = pandas.merge(left=links_df[["A_B","B"]], right=nodes_df[["N","X","Y"]], left_on="B", right_on="N", how="left")
-
-        assert( len(link_A_points_df.loc[ pandas.isnull(link_A_points_df.X) ]) == 0)
-        assert( len(link_A_points_df.loc[ pandas.isnull(link_B_points_df.X) ]) == 0)
-
-        # sort field
-        link_A_points_df["node"] = 1
-        link_B_points_df["node"] = 2
-        link_points_df = pandas.concat([link_A_points_df, link_B_points_df], axis="index")
-        link_points_df.reset_index(drop=True, inplace=True)
-        logging.debug("\n{0}".format(link_points_df.head()))
-
-        link_points_arr = numpy.array(link_points_df[["A_B","N","X","Y","node"]].to_records(),
-                                      numpy.dtype([('index',numpy.uint32),
-                                                   ('A_B',"U21"),
-                                                   ('N',numpy.uint32),
-                                                   ('X',numpy.double),
-                                                   ('Y',numpy.double),
-                                                   ('node',numpy.int8)]))
-
-        # convert to a Table
-        link_points_table = LINK_POINTS_DBF
-        arcpy.da.NumPyArrayToTable(link_points_arr, os.path.join(WORKING_DIR,link_points_table))
-        logging.info("Created table {0}".format(link_points_table))
-
-        # create points
-        link_points_layer = "link_points_layer"
-        arcpy.MakeXYEventLayer_management(table=link_points_table, in_x_field="X", in_y_field="Y",
-                                          out_layer=link_points_layer, spatial_reference=os.path.join(WORKING_DIR, PROJ_FILE))
-        logging.info("Made layer {0}".format(link_points_layer))
-
-        arcpy.PointsToLine_management(link_points_layer, LINK_NOATTRS_SHP, "A_B", "node")
-        logging.info("Created initial links in {0}".format(LINK_NOATTRS_SHP))
-
-        # join it to the rest of the attributes
-        link_layer = "link_layer"
-        arcpy.MakeFeatureLayer_management(LINK_NOATTRS_SHP, link_layer)
-        arcpy.AddJoin_management(link_layer, "A_B", LINK_DBFFILE, "A_B")
-        logging.info("Joined with link attributes")
-
-        #  list the link fields
-        link_fields = arcpy.ListFields(link_layer)
-        rename_dict = {}
-        for field in link_fields:
-            logging.info("  {0:50s} is a type of {1:15s} with a length of {2}".format(field.name, field.type, field.length))
-            if field.name.startswith("links."):
-                # this is odd, renaming a field to itself, but that's because the default is to rename to links.XXX
-                rename_dict[field.name[6:]] = [field.name[6:], field.name[6:]]
-        # print(rename_dict)
-
-        # drop a couple useless fields
-        arcpy.DeleteField_management(link_layer, ["Id"])
-
-        # rename the other fields to their original
-        rename_fields(link_layer, LINK_SHPFILE, rename_dict)
-        logging.info("Created links in {0}".format(LINK_SHPFILE))
-
-    except Exception as err:
-        logging.error(err.args[0])
-        traceback.print_exc(file=sys.stdout)
-
-    # clean up temporary files
-    try:
-        arcpy.Delete_management(LINK_POINTS_DBF)
-    except Exception as err:
-        logging.debug(err.args[0])
-
-    try:
-        arcpy.Delete_management(LINK_NOATTRS_SHP)
-    except Exception as err:
-        logging.debug(err.args[0])
