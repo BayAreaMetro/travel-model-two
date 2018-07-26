b5486f6697946bcd38a63521f0c48d26e0f389e5
Lisa Zorn <lzorn@bayareametro.gov>
2018-07-26 11:38:31 -0700

    Add option to split transit shapefile by operator

--- utilities/cube_to_shapefile.py
+++ utilities/cube_to_shapefile.py
@@ -4,7 +4,7 @@ Create shapefile of Cube network, roadway and transit.
 
 """
 
-import argparse, csv, logging, os, subprocess, sys, traceback
+import argparse, collections, csv, logging, os, subprocess, sys, traceback
 import numpy, pandas
 import Wrangler
 
@@ -15,9 +15,29 @@ LOG_FILE        = "cube_to_shapefile.log"
 NODE_SHPFILE    = "network_nodes.shp"
 LINK_SHPFILE    = "network_links.shp"
 
-TRN_LINES_SHPFILE = "network_trn_lines.shp"
-TRN_LINKS_SHPFILE = "network_trn_links.shp"
-TRN_STOPS_SHPFILE = "network_trn_stops.shp"
+TRN_LINES_SHPFILE = "network_trn_lines{}.shp"
+TRN_LINKS_SHPFILE = "network_trn_links{}.shp"
+TRN_STOPS_SHPFILE = "network_trn_stops{}.shp"
+
+TRN_OPERATORS = collections.OrderedDict([
+    # filename_append       # list of operator text
+    ("_SF_Muni",             ["San Francisco MUNI"]),
+    ("_SC_VTA",              ["Santa Clara VTA"]),
+    ("_AC_Transit",          ["AC Transit", "AC Transbay"]),
+    ("_SM_SamTrans",         ["samTrans"]),
+    ("_SC_Transit",          ["Sonoma County Transit"]),
+    ("_CC_CountyConnection", ["The County Connection"]),
+    ("_GG_Transit",          ["Golden Gate Transit", "Golden Gate Ferry"]),
+    ("_WestCAT",             ["WestCAT"]),
+    ("_WHEELS",              ["WHEELS"]),
+    ("_Stanford",            ["Stanford Marguerite Shuttle"]),
+    ("_TriDelta",            ["TriDelta Transit"]),
+    ("_Caltrain",            ["Caltrain"]),
+    ("_BART",                ["BART"]),
+    ("_ferry",               ["Alameda Harbor Bay Ferry", "Alameda/Oakland Ferry","Angel Island - Tiburon Ferry",
+                              "Oakland/South SSF Ferry", "South SF/Oakland Ferry", "Vallejo Baylink Ferry"]),
+    ("_other" ,              []),  # this will get filled in with operators not covered already
+])
 
 def runCubeScript(workingdir, script_filename, script_env):
     """
@@ -90,7 +110,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.RawDescriptionHelpFormatter,)
     parser.add_argument("netfile",  metavar="network.net", nargs=1, help="Cube input roadway network file")
     parser.add_argument("--linefile", metavar="transit.lin", nargs=1, help="Cube input transit line file", required=False)
+    parser.add_argument("--by_operator", action="store_true", help="Split transit lines by operator")
     args = parser.parse_args()
+    print(args)
 
     # setup the environment
     script_env                 = os.environ.copy()
@@ -100,7 +122,7 @@ if __name__ == '__main__':
     script_env["LINK_OUTFILE"] = LINK_SHPFILE
 
     # run the script to do the work
-    runCubeScript(WORKING_DIR, os.path.join(CODE_DIR, "export_network.job"), script_env)
+    # runCubeScript(WORKING_DIR, os.path.join(CODE_DIR, "export_network.job"), script_env)
     print("Wrote network node file to {}".format(NODE_SHPFILE))
     print("Wrote network link file to {}".format(LINK_SHPFILE))
 
@@ -116,46 +138,65 @@ if __name__ == '__main__':
     # if we don't have a transit file, then we're done
     if not args.linefile: sys.exit(0)
 
-    # delete shapefiles if one exists already
-    arcpy.Delete_management(TRN_LINES_SHPFILE)
-    arcpy.Delete_management(TRN_LINKS_SHPFILE)
-    arcpy.Delete_management(TRN_STOPS_SHPFILE)
+    operator_files = [""]
+    if args.by_operator:
+        operator_files = list(TRN_OPERATORS.keys())
 
-    # create the lines shapefile
-    arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_LINES_SHPFILE, "POLYLINE")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "NAME", "TEXT", field_length=25)
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "HEADWAY_EA", "FLOAT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "HEADWAY_AM", "FLOAT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "HEADWAY_MD", "FLOAT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "HEADWAY_PM", "FLOAT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "HEADWAY_EV", "FLOAT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "MODE",       "SHORT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "MODE_TYPE",  "TEXT", field_length=15)
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "OPERATOR_T", "TEXT", field_length=40)
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "VEHICLETYP","SHORT")
-    arcpy.AddField_management(TRN_LINES_SHPFILE, "OPERATOR",   "SHORT")
+    # store cursors here
+    line_cursor      = {}
+    link_cursor      = {}
+    stop_cursor      = {}
+    operator_to_file = {}
 
-    line_cursor = arcpy.da.InsertCursor(TRN_LINES_SHPFILE, ["NAME", "SHAPE@",
+    for operator_file in operator_files:
+        if args.by_operator:
+            for op_txt in TRN_OPERATORS[operator_file]:
+                operator_to_file[op_txt] = operator_file
+
+        # delete shapefiles if one exists already
+        arcpy.Delete_management(TRN_LINES_SHPFILE.format(operator_file))
+        arcpy.Delete_management(TRN_LINKS_SHPFILE.format(operator_file))
+        arcpy.Delete_management(TRN_STOPS_SHPFILE.format(operator_file))
+
+        # create the lines shapefile
+        arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_LINES_SHPFILE.format(operator_file), "POLYLINE")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "NAME", "TEXT", field_length=25)
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "HEADWAY_EA", "FLOAT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "HEADWAY_AM", "FLOAT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "HEADWAY_MD", "FLOAT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "HEADWAY_PM", "FLOAT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "HEADWAY_EV", "FLOAT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "MODE",       "SHORT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "MODE_TYPE",  "TEXT", field_length=15)
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "OPERATOR_T", "TEXT", field_length=40)
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "VEHICLETYP","SHORT")
+        arcpy.AddField_management(TRN_LINES_SHPFILE.format(operator_file), "OPERATOR",   "SHORT")
+
+        line_cursor[operator_file] = arcpy.da.InsertCursor(TRN_LINES_SHPFILE.format(operator_file), ["NAME", "SHAPE@",
                                    "HEADWAY_EA", "HEADWAY_AM", "HEADWAY_MD", "HEADWAY_PM", "HEADWAY_EV",
                                    "MODE", "MODE_TYPE", "OPERATOR_T", "VEHICLETYP", "OPERATOR"])
 
-    # create the lines shapefile
-    arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_LINKS_SHPFILE, "POLYLINE")
-    arcpy.AddField_management(TRN_LINKS_SHPFILE, "NAME",    "TEXT", field_length=25)
-    arcpy.AddField_management(TRN_LINKS_SHPFILE, "A",       "LONG")
-    arcpy.AddField_management(TRN_LINKS_SHPFILE, "B",       "LONG")
-    arcpy.AddField_management(TRN_LINKS_SHPFILE, "SEQ",     "SHORT")
+        # create the lines shapefile
+        arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_LINKS_SHPFILE.format(operator_file), "POLYLINE")
+        arcpy.AddField_management(TRN_LINKS_SHPFILE.format(operator_file), "NAME",    "TEXT", field_length=25)
+        arcpy.AddField_management(TRN_LINKS_SHPFILE.format(operator_file), "A",       "LONG")
+        arcpy.AddField_management(TRN_LINKS_SHPFILE.format(operator_file), "B",       "LONG")
+        arcpy.AddField_management(TRN_LINKS_SHPFILE.format(operator_file), "SEQ",     "SHORT")
 
-    link_cursor = arcpy.da.InsertCursor(TRN_LINKS_SHPFILE, ["NAME", "SHAPE@", "A", "B", "SEQ"])
+        link_cursor[operator_file] = arcpy.da.InsertCursor(TRN_LINKS_SHPFILE.format(operator_file),
+                                                           ["NAME", "SHAPE@", "A", "B", "SEQ"])
 
-    # create the stops shapefile
-    arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_STOPS_SHPFILE, "POINT")
-    arcpy.AddField_management(TRN_STOPS_SHPFILE, "NAME",     "TEXT", field_length=25)
-    arcpy.AddField_management(TRN_STOPS_SHPFILE, "N",        "LONG")
-    arcpy.AddField_management(TRN_STOPS_SHPFILE, "SEQ",      "SHORT")
-    arcpy.AddField_management(TRN_STOPS_SHPFILE, "IS_STOP",  "SHORT")
+        # create the stops shapefile
+        arcpy.CreateFeatureclass_management(WORKING_DIR, TRN_STOPS_SHPFILE.format(operator_file), "POINT")
+        arcpy.AddField_management(TRN_STOPS_SHPFILE.format(operator_file), "NAME",     "TEXT", field_length=25)
+        arcpy.AddField_management(TRN_STOPS_SHPFILE.format(operator_file), "N",        "LONG")
+        arcpy.AddField_management(TRN_STOPS_SHPFILE.format(operator_file), "SEQ",      "SHORT")
+        arcpy.AddField_management(TRN_STOPS_SHPFILE.format(operator_file), "IS_STOP",  "SHORT")
 
-    stop_cursor = arcpy.da.InsertCursor(TRN_STOPS_SHPFILE, ["NAME", "SHAPE@", "N", "SEQ", "IS_STOP"])
+        stop_cursor[operator_file] = arcpy.da.InsertCursor(TRN_STOPS_SHPFILE.format(operator_file),
+                                                           ["NAME", "SHAPE@", "N", "SEQ", "IS_STOP"])
+
+    print(operator_to_file)
 
     # read the node points
     nodes_array = arcpy.da.TableToNumPyArray(in_table="{}.DBF".format(NODE_SHPFILE[:-4]),
@@ -179,7 +220,21 @@ if __name__ == '__main__':
         link_point_array = arcpy.Array()
         last_n = -1
         seq    = 1
+        op_txt = line.attr['USERA1'].strip('\""')
 
+        if not args.by_operator:
+            operator_file = ""
+        elif op_txt in operator_to_file:
+            operator_file = operator_to_file[op_txt]
+        else:
+            operator_file = "_other"
+            operator_to_file[op_txt] = operator_file
+            if op_txt not in TRN_OPERATORS[operator_file]:
+                TRN_OPERATORS[operator_file].append(op_txt)
+
+        print("Adding line {:4}/{:4} {:25} operator {:40} to operator_file [{}]".format(
+              line_count+1,len(trn_net.lines),
+              line.name, op_txt, operator_file))
         # for attr_key in line.attr: print(attr_key, line.attr[attr_key])
 
         for node in line.n:
@@ -189,7 +244,7 @@ if __name__ == '__main__':
             point = arcpy.Point( nodes_x[n], nodes_y[n] )
 
             # start at 0 for stops
-            stop_cursor.insertRow([line.name, point, n, seq-0, is_stop])
+            stop_cursor[operator_file].insertRow([line.name, point, n, seq-0, is_stop])
             stop_count += 1
 
             # add to line array
@@ -198,8 +253,8 @@ if __name__ == '__main__':
             link_point_array.add(point)
             if link_point_array.count > 1:
                 plink_shape = arcpy.Polyline(link_point_array)
-                link_cursor.insertRow([line.name, plink_shape,
-                                      last_n, n, seq])
+                link_cursor[operator_file].insertRow([line.name, plink_shape,
+                                                      last_n, n, seq])
                 link_count += 1
                 link_point_array.remove(0)
 
@@ -207,18 +262,18 @@ if __name__ == '__main__':
             seq += 1
 
         pline_shape = arcpy.Polyline(line_point_array)
-        line_cursor.insertRow([line.name, pline_shape,
-                          float(line.attr['HEADWAY[1]']),
-                          float(line.attr['HEADWAY[2]']),
-                          float(line.attr['HEADWAY[3]']),
-                          float(line.attr['HEADWAY[4]']),
-                          float(line.attr['HEADWAY[5]']),
-                          line.attr['MODE'],
-                          line.attr['USERA2'].strip('\""'), # mode type
-                          line.attr['USERA1'].strip('\""'), # operator
-                          line.attr['VEHICLETYPE'],
-                          line.attr['OPERATOR']
-                         ])
+        line_cursor[operator_file].insertRow([line.name, pline_shape,
+                                              float(line.attr['HEADWAY[1]']),
+                                              float(line.attr['HEADWAY[2]']),
+                                              float(line.attr['HEADWAY[3]']),
+                                              float(line.attr['HEADWAY[4]']),
+                                              float(line.attr['HEADWAY[5]']),
+                                              line.attr['MODE'],
+                                              line.attr['USERA2'].strip('\""'), # mode type
+                                              op_txt, # operator
+                                              line.attr['VEHICLETYPE'],
+                                              line.attr['OPERATOR']
+                                            ])
         line_count += 1
 
     del stop_cursor
