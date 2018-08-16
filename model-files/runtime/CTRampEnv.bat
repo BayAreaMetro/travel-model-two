72f46fb5021655ce7aa3b8de61299b133dcd1029
Lisa Zorn <lzorn@bayareametro.gov>
2018-08-16 16:35:58 -0700

    Create ENVTYPE for RSG/MTC
    So paths can differ

--- model-files/runtime/CTRampEnv.bat
+++ model-files/runtime/CTRampEnv.bat
@@ -1,18 +1,29 @@
-rem this file has environment variables for CT-RAMP batch files
+:: This file sets up environment variables for CT-RAMP batch files
+::
+::   JAVA_PATH: location of the 64-bit java development kit or runtime environment
+::    TPP_PATH: location of the RUNTPP and VOYAGER executables from Citilabs
+::   CUBE_PATH: location of the Cube executable from Citilabs
+:: PYTHON_PATH: location of the Python executable
+::     RUNTIME: location of the main JAR file
+::  GITHUB_DIR: location of the github travel-model-two repo
 
-:: The location of the 64-bit java development kit or runtime environment
-set JAVA_PATH=C:\Program Files\Java\jre1.8.0_131
+IF %ENVTYPE%==RSG (
+  set "JAVA_PATH=C:\Program Files\Java\jre1.8.0_131"
+  set "TPP_PATH=C:\Program Files\Citilabs\CubeVoyager"
+  set "CUBE_PATH=C:\Program Files (x86)\Citilabs\Cube"
+  set "CUBE_DLL_PATH=C:\Program Files\Citilabs\VoyagerFileAPI"
+  set "PYTHON_PATH=C:\Program Files\anaconda2"
+  set "GITHUB_DIR=C:\Users\mtcpb\Documents\GitHub\travel-model-two"
+)
+IF %ENVTYPE%==MTC (
+  set "JAVA_PATH=C:\Program Files\Java\jdk1.8.0_181"
+  set "TPP_PATH=C:\Program Files\Citilabs\CubeVoyager"
+  set "CUBE_PATH=C:\Program Files (x86)\Citilabs\Cube"
+  set "CUBE_DLL_PATH=C:\Program Files\Citilabs\VoyagerFileAPI"
+  set "PYTHON_PATH=C:\Python27"
+  set "GITHUB_DIR=C:\Users\mtcpb\Documents\GitHub\travel-model-two"
+)
 
-:: The location of the RUNTPP executable from Citilabs
-set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager
-
-:: The location of the Cube executable from Citilabs
-set CUBE_PATH=C:\Program Files (x86)\Citilabs\Cube
-
-:: The location of the Python executable
-set PYTHON_PATH=C:\Program Files\anaconda2
-
-:: The location of the main JAR file
 set RUNTIME=CTRAMP/runtime
 
 rem set ports
@@ -21,13 +32,9 @@ set MATRIX_MANAGER_PORT=1191
 set HH_MANAGER_PORT=1129
 
 rem set machine names
-SET MAIN=PORMDLPPW02
-rem SET MTC01=W-AMPDX-D-SAG01
-SET MTC02=PORMDLPPW02
-rem SET MTC03=W-AMPDX-D-SAG10
+SET MAIN=model2-b
+SET MTC02=model2-b
 
-rem SET node_runner_MAIN=runMtc04
-rem SET node_runner_MTC01=runMtc01
 SET node_runner_MTC02=runMtc02
 rem set IP addresses
 :: get the ipaddress of this machine
@@ -40,28 +47,28 @@ FOR %%A IN (%IP%) DO SET IPADDRESS=%%A
 ::set HOST_IP_ADDRESS=10.70.192.64
 SET HOST_IP_ADDRESS=%IPADDRESS%
 
-set HHMGR_IP=172.24.0.102
+set HHMGR_IP=192.168.1.206
 
 :: Machine running matrix data manager
 SET MATRIX_SERVER=\\%MTC02%
-SET MATRIX_SERVER_BASE_DIR=%MATRIX_SERVER%\e$\projects\clients\MTC\%SCEN%
-SET MATRIX_SERVER_ABSOLUTE_BASE_DIR=e:\projects\clients\MTC\%SCEN%
-SET MATRIX_SERVER_JAVA_PATH=C:\Program Files\Java\jre1.8.0_131
+SET MATRIX_SERVER_BASE_DIR=\\model2-a\Model2a-Share\Projects_TM2\%SCEN%
+SET MATRIX_SERVER_ABSOLUTE_BASE_DIR=X:\Projects_TM2\%SCEN%
+SET MATRIX_SERVER_JAVA_PATH=%JAVA_PATH%
 
 :: Machine running household data manager
 SET HH_SERVER=\\%MTC02%
-SET HH_SERVER_BASE_DIR=%HH_SERVER%\e$\projects\clients\MTC\%SCEN%
-SET HH_SERVER_ABSOLUTE_BASE_DIR=e:\projects\clients\MTC\%SCEN%
-SET HH_SERVER_JAVA_PATH=C:\Program Files\Java\jre1.8.0_131
+SET HH_SERVER_BASE_DIR=\\model2-a\Model2a-Share\Projects_TM2\%SCEN%
+SET HH_SERVER_ABSOLUTE_BASE_DIR=X:\Projects_TM2\%SCEN%
+SET HH_SERVER_JAVA_PATH=%JAVA_PATH%
 
 rem set main property file name
 set PROPERTIES_NAME=sandag_abm
 
 rem all nodes need to map the scenario drive, currently mapped as Q:
-set MAPDRIVE=R:
+set MAPDRIVE=X:
 rem uncomment next line if use T drive as data folder.  
 rem !!!Note: much slower than a local data folder!!!
-set MAPDRIVEFOLDER=\\%MAIN%\projects\clients\MTC\%SCEN%
+set MAPDRIVEFOLDER=\\%MAIN%\projects_tm2\%SCEN%
 
 rem location of mapThenRun.bat on remote machines
 set MAPANDRUN=CTRAMP\runtime\mapThenRunNew.bat
