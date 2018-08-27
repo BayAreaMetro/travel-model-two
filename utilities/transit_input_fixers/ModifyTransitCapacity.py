b9cc51109a8b8030f8da1697abe13af48139d2dd
Flavia <flavia.tsang@gmail.com>
2018-08-27 08:56:38 -0700

    Script to modify transit capacity - added options for dealing with blanks in the input excel file

--- utilities/transit_input_fixers/ModifyTransitCapacity.py
+++ utilities/transit_input_fixers/ModifyTransitCapacity.py
@@ -6,6 +6,10 @@ USAGE = """
 
 Script to write vehicle type (transit capacity) information from an excel file to the transit line file
 
+Note there are two user options for dealing with blanks in the input excel file:
+- 1: if the vehicle type is blank in the input excel file, assume it is a standard bus
+- 2: if the vehicle type is blank in the input excel file, assume the vehicle type to stay the same as the input line file
+
 """
 
 USERNAME     = os.environ["USERNAME"]
@@ -21,6 +25,11 @@ trn_net = Wrangler.TransitNetwork(champVersion=4.3, basenetworkpath=TRN_NETFILE,
 VehicleType_df = pandas.read_excel(r"M:\\Development\\Travel Model Two\\Supply\\Transit\\Network_QA\\Line and vehicle type_be.xlsx", sheet_name='transit_input_summary')
 
 
+# options for dealing with blanks in the input excel file:
+# - 1: if the vehicle type is blank in the input excel file, assume it is a standard bus
+# - 2: if the vehicle type is blank in the input excel file, assume the vehicle type to stay the same as the input line file
+Option_for_blanks = 1
+
 for line in trn_net:
     for i in VehicleType_df.index:
 
@@ -32,19 +41,22 @@ for line in trn_net:
         if line_name_linefile == line_name_excel:
 
 	    # if the vehicle type is blank, then assume it is a standard bus
-            if  str(VehicleType_df['Vehicle Type'][i]) =="nan":
+            if  (str(VehicleType_df['Vehicle Type'][i]) =="nan") and (Option_for_blanks==1):
 
                 line['VEHICLETYPE'] = 1
-                print("Changed:",i, "Line name excel-", line.name, "Line name in line file-", VehicleType_df['Line_name'][i], "Vehicle type-", line['VEHICLETYPE'])
-
-                break
 
             else:
 
                 line['VEHICLETYPE'] = int(VehicleType_df['Vehicle Type'][i])
-                print("Changed:",i, "Line name excel-", line.name, "Line name in line file-", VehicleType_df['Line_name'][i], "Vehicle type-", line['VEHICLETYPE'])
 
-                continue
+        
+            print("Changed:",i, "Line name excel-", line.name, "Line name in line file-", VehicleType_df['Line_name'][i], "Vehicle type-", line['VEHICLETYPE'])
+            break
+
+
+        else:
+
+            continue
 
 
 trn_net.write(path=".", name="transitLines", writeEmptyFiles=False, suppressValidation=True)
