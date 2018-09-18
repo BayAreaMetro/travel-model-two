db2d58356084d4080b95a87de4793b85acecb80f
khademul haque <khademul144@gmail.com>
2018-09-17 17:10:20 -0700

    Additional fixes to the highway validation summary script

--- model-files/verification/hwyValidation.R
+++ model-files/verification/hwyValidation.R
@@ -102,6 +102,88 @@ write.csv(volumes[volumes$PEMSID>0, ], file.path(volumeDir, "total_volumes.csv")
 volumes     <- read.csv(file.path(volumeDir, "total_volumes.csv"), as.is = T)
 volumes_pems <- volumes
 
+## processing the pems_period data to subset only the latest year PEMS count
+pems_period <- pems_period %>%
+  mutate(station_year = paste(as.character(station), as.character(year), sep = "_"))
+pems_period_latestyear <- aggregate(year ~ station, pems_period, function(x) max(x))
+pems_period_latestyear <- pems_period_latestyear %>% 
+  mutate(station_year = paste(as.character(station), as.character(year), sep = "_"))
+pems_period_final <- pems_period %>% 
+  filter(station_year %in% pems_period_latestyear$station_year)
+
+## check for those PEMSID whose latest year do not have all time periods. Fix by taking the next latest year
+check_pems <- data.frame(table(pems_period_final$station_year))
+pems_2ndyear <- check_pems %>% 
+  filter(Freq<5)
+pems_2ndyear_stations <- pems_period_latestyear %>% 
+  filter(station_year %in% pems_2ndyear$Var1)
+pems_2ndyear_all <- pems_period %>% 
+  filter(station %in% pems_2ndyear_stations$station)
+pems_2ndyear_all <- pems_2ndyear_all %>% 
+  select(station, year) %>% 
+  mutate(counter=1)
+
+pems_2ndyear_pivot <- dcast(pems_2ndyear_all, station ~ year, value.var = "counter", fun.aggregate = length)
+pems_2ndyear_pivot <- pems_2ndyear_pivot %>% 
+  mutate(ind_2005=if_else(`2005`==5,1,0)) %>%
+  mutate(ind_2006=if_else(`2006`==5,1,0)) %>%
+  mutate(ind_2007=if_else(`2007`==5,1,0)) %>%
+  mutate(ind_2008=if_else(`2008`==5,1,0)) %>%
+  mutate(ind_2009=if_else(`2009`==5,1,0)) %>%
+  mutate(ind_2010=if_else(`2010`==5,1,0)) %>%
+  mutate(ind_2011=if_else(`2011`==5,1,0)) %>%
+  mutate(ind_2012=if_else(`2012`==5,1,0)) %>%
+  mutate(ind_2013=if_else(`2013`==5,1,0)) %>%
+  mutate(ind_2014=if_else(`2014`==5,1,0)) %>%
+  mutate(ind_2015=if_else(`2015`==5,1,0)) %>% 
+  mutate(final_year=if_else(ind_2015==1,2015,if_else(ind_2014==1,2014,if_else(ind_2013==1,2013,if_else(ind_2012==1,2012,if_else(ind_2011==1,2011,if_else(ind_2010==1,2010,if_else(ind_2009==1,2009,if_else(ind_2008==1,2008,if_else(ind_2007==1,2007,if_else(ind_2006==1,2006,if_else(ind_2005==1,2005,0))))))))))))
+pems_2ndyear_final <- pems_2ndyear_pivot %>% 
+  select(station, final_year)
+pems_period_final <- pems_period_final %>% 
+  filter(!(station %in% pems_2ndyear_final$station))
+pems_2ndyear_final <- pems_2ndyear_final %>% 
+  filter(final_year>0) %>% 
+  mutate(station_year = paste(as.character(station), as.character(final_year), sep = "_"))
+pems_period_final_filtered <- pems_period %>% 
+  filter(station_year %in% pems_2ndyear_final$station_year)
+pems_period_test <- merge(pems_period_final, pems_period_final_filtered, all = T)
+
+## manually correct PEMSID with multiple time period data for same year
+check_pems2 <- check_pems %>% 
+  filter(Freq>5)
+pems_period_multiPEMS <- pems_period_test %>% 
+  filter(station_year %in% check_pems2$Var1)
+station_400040 <- pems_period_multiPEMS %>% 
+  filter(station==400040) %>% 
+  filter(lanes>2)
+station_400236 <- pems_period_multiPEMS %>% 
+  filter(station==400236) %>% 
+  filter(lanes>2)
+station_400521 <- pems_period_multiPEMS %>% 
+  filter(station==400521) %>% 
+  filter(lanes>2)
+station_400596 <- pems_period_multiPEMS %>% 
+  filter(station==400596) %>% 
+  filter(lanes>2)
+station_401433 <- pems_period_multiPEMS %>% 
+  filter(station==401433) %>% 
+  filter(lanes>4)
+station_402536 <- pems_period_multiPEMS %>% 
+  filter(station==402536) %>% 
+  filter(type=="ML")
+station_404462 <- pems_period_multiPEMS %>% 
+  filter(station==404462) %>% 
+  filter(lanes>2)
+station_407332 <- pems_period_multiPEMS %>% 
+  filter(station==407332) %>% 
+  filter(type=="HV")
+station_407359 <- pems_period_multiPEMS %>% 
+  filter(station==407359) %>% 
+  filter(type=="HV")
+pems_period_test_2 <- merge(station_400040, merge(station_400236, merge(station_400521, merge(station_400596, merge(station_401433, merge(station_402536, merge(station_404462, merge(station_407332, station_407359, all = T), all = T), all = T), all = T), all = T), all = T), all = T), all = T)
+pems_period_test <- pems_period_test %>% 
+  filter(!(station_year %in% check_pems2$Var1))
+pems_period <- merge(pems_period_test, pems_period_test_2, all=T)
 
 # subset pems by period
 pems_EA <- pems_period[pems_period$time_period=="EA",]
@@ -117,36 +199,149 @@ volumes_pems$pems_PM <- pems_PM$avg_flow[match(volumes_pems$PEMSID, pems_PM$stat
 volumes_pems$pems_EV <- pems_EV$avg_flow[match(volumes_pems$PEMSID, pems_EV$station)]
 volumes_pems$year <- pems_period$year[match(volumes_pems$PEMSID, pems_period$station)]
 
-volumes_pems1 <- select(volumes_pems, A, B, OLD_A, OLD_B, PEMSID, FT, year, VolEA_tot, VolAM_tot, VolMD_tot, VolPM_tot, VolEV_tot)
-colnames(volumes_pems1) <- c("a","b", "old_a", "old_b","pemsid","ft", "year", "EA", "AM", "MD", "PM", "EV")
-volumes_pems1[is.na(volumes_pems1)] <- 0
-volumes_pems1 <- volumes_pems1 %>%
-  mutate(Total = EA+AM+MD+PM+EV)
-temp1 <- melt(volumes_pems1, id = c("a", "b", "old_a", "old_b", "pemsid", "ft", "year"))
-colnames(temp1) <- c("a","b", "old_a", "old_b","pemsid","ft", "year", "time_period", "volume")
-volumes_pems2 <- select(volumes_pems, A, B, OLD_A, OLD_B, PEMSID, FT, year, pems_EA, pems_AM, pems_MD, pems_PM, pems_EV)
-colnames(volumes_pems2) <- c("a","b", "old_a", "old_b","pemsid","ft", "year", "EA", "AM", "MD", "PM", "EV")
-volumes_pems2[is.na(volumes_pems2)] <- 0
-volumes_pems2 <- volumes_pems2 %>%
-  mutate(Total = EA+AM+MD+PM+EV)
-temp2 <- melt(volumes_pems2, id = c("a", "b", "old_a", "old_b", "pemsid", "ft", "year"))
-colnames(temp2) <- c("a","b", "old_a", "old_b","pemsid","ft", "year", "time_period", "count")
+# changes: keep links with counts > 0.
+# calculate total based on all time periods
+
+## Delete all rows with no count data
+volumes_pems <- volumes_pems %>% 
+  filter(!is.na(year))
+volumes_pems <- volumes_pems %>% 
+  mutate(VolAM_tot2=if_else(is.na(pems_AM),99999,VolAM_tot)) %>% 
+  mutate(VolEA_tot2=if_else(is.na(pems_EA),99999,VolEA_tot)) %>% 
+  mutate(VolMD_tot2=if_else(is.na(pems_MD),99999,VolMD_tot)) %>% 
+  mutate(VolPM_tot2=if_else(is.na(pems_PM),99999,VolPM_tot)) %>% 
+  mutate(VolEV_tot2=if_else(is.na(pems_EV),99999,VolEV_tot))
+volumes_pems$VolAM_tot2[volumes_pems$VolAM_tot2 == 99999] <- NA
+volumes_pems$VolEA_tot2[volumes_pems$VolEA_tot2 == 99999] <- NA
+volumes_pems$VolMD_tot2[volumes_pems$VolMD_tot2 == 99999] <- NA
+volumes_pems$VolPM_tot2[volumes_pems$VolPM_tot2 == 99999] <- NA
+volumes_pems$VolEV_tot2[volumes_pems$VolEV_tot2 == 99999] <- NA
+
+volumes_pems1 <- select(volumes_pems, A, B, OLD_A, OLD_B, OLD_A_B, PEMSID, FT, year, VolEA_tot2, VolAM_tot2, VolMD_tot2, VolPM_tot2, VolEV_tot2)
+colnames(volumes_pems1) <- c("a","b", "old_a", "old_b", "old_a_b", "pemsid","ft", "year", "EA", "AM", "MD", "PM", "EV")
+volumes_pems1 <- volumes_pems1 %>% 
+  rowwise() %>% 
+  mutate(Total=sum(EA,AM,MD,PM,EV, na.rm=TRUE)) %>% 
+  ungroup()
+temp1 <- melt(data.frame(volumes_pems1), id = c("a", "b", "old_a", "old_b", "old_a_b", "pemsid", "ft", "year"))
+colnames(temp1) <- c("a","b", "old_a", "old_b", "old_a_b", "pemsid","ft", "year", "time_period", "volume")
+
+volumes_pems2 <- select(volumes_pems, A, B, OLD_A, OLD_B, OLD_A_B, PEMSID, FT, year, pems_EA, pems_AM, pems_MD, pems_PM, pems_EV)
+colnames(volumes_pems2) <- c("a","b", "old_a", "old_b", "old_a_b", "pemsid","ft", "year", "EA", "AM", "MD", "PM", "EV")
+volumes_pems2 <- volumes_pems2 %>% 
+  rowwise() %>% 
+  mutate(Total=sum(EA,AM,MD,PM,EV, na.rm=TRUE)) %>% 
+  ungroup()
+temp2 <- melt(data.frame(volumes_pems2), id = c("a", "b", "old_a", "old_b", "old_a_b", "pemsid", "ft", "year"))
+colnames(temp2) <- c("a","b", "old_a", "old_b", "old_a_b", "pemsid","ft", "year", "time_period", "count")
 
 comparison <- data.frame(temp1, temp2$count)
-colnames(comparison) <- c("a","b", "old_a", "old_b","pemsid","ft", "year","time_period","volume","count")
+colnames(comparison) <- c("a","b", "old_a", "old_b", "old_a_b", "pemsid","ft", "year","time_period","volume","count")
 comparison[is.na(comparison)] <- 0
 comparison <- comparison %>%
-  filter(volume>0 & count>0)
+  filter(count>0)
+
+# manually fix links with multiple PEMSID based on lowest count volume difference
+comparison_EA <- comparison[comparison$time_period=="EA",]
+comparison_AM <- comparison[comparison$time_period=="AM",]
+comparison_MD <- comparison[comparison$time_period=="MD",]
+comparison_PM <- comparison[comparison$time_period=="PM",]
+comparison_EV <- comparison[comparison$time_period=="EV",]
+comparison_total <- comparison[comparison$time_period=="Total",]
+
+# EA
+dup_links_EA <- data.frame(table(comparison_EA$old_a_b))
+dup_links_EA <- dup_links_EA %>% 
+  filter(Freq>1)
+comparison_EA_dup <- comparison_EA %>% 
+  filter(old_a_b %in% dup_links_EA$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_EA_dup2 <- comparison_EA_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_EA_dup2$gap <- NULL
+comparison_EA1 <- comparison_EA %>% 
+  filter(!(old_a_b %in% comparison_EA_dup2$old_a_b))
+comparison_EA <- merge(comparison_EA1, comparison_EA_dup2, all=T)
+
+# AM
+dup_links_AM <- data.frame(table(comparison_AM$old_a_b))
+dup_links_AM <- dup_links_AM %>% 
+  filter(Freq>1)
+comparison_AM_dup <- comparison_AM %>% 
+  filter(old_a_b %in% dup_links_AM$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_AM_dup2 <- comparison_AM_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_AM_dup2$gap <- NULL
+comparison_AM1 <- comparison_AM %>% 
+  filter(!(old_a_b %in% comparison_AM_dup2$old_a_b))
+comparison_AM <- merge(comparison_AM1, comparison_AM_dup2, all=T)
+
+# MD
+dup_links_MD <- data.frame(table(comparison_MD$old_a_b))
+dup_links_MD <- dup_links_MD %>% 
+  filter(Freq>1)
+comparison_MD_dup <- comparison_MD %>% 
+  filter(old_a_b %in% dup_links_MD$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_MD_dup2 <- comparison_MD_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_MD_dup2$gap <- NULL
+comparison_MD1 <- comparison_MD %>% 
+  filter(!(old_a_b %in% comparison_MD_dup2$old_a_b))
+comparison_MD <- merge(comparison_MD1, comparison_MD_dup2, all=T)
+
+# PM
+dup_links_PM <- data.frame(table(comparison_PM$old_a_b))
+dup_links_PM <- dup_links_PM %>% 
+  filter(Freq>1)
+comparison_PM_dup <- comparison_PM %>% 
+  filter(old_a_b %in% dup_links_PM$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_PM_dup2 <- comparison_PM_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_PM_dup2$gap <- NULL
+comparison_PM1 <- comparison_PM %>% 
+  filter(!(old_a_b %in% comparison_PM_dup2$old_a_b))
+comparison_PM <- merge(comparison_PM1, comparison_PM_dup2, all=T)
+
+# EV
+dup_links_EV <- data.frame(table(comparison_EV$old_a_b))
+dup_links_EV <- dup_links_EV %>% 
+  filter(Freq>1)
+comparison_EV_dup <- comparison_EV %>% 
+  filter(old_a_b %in% dup_links_EV$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_EV_dup2 <- comparison_EV_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_EV_dup2$gap <- NULL
+comparison_EV1 <- comparison_EV %>% 
+  filter(!(old_a_b %in% comparison_EV_dup2$old_a_b))
+comparison_EV <- merge(comparison_EV1, comparison_EV_dup2, all=T)
+
+# Total
+dup_links_total <- data.frame(table(comparison_total$old_a_b))
+dup_links_total <- dup_links_total %>% 
+  filter(Freq>1)
+comparison_total_dup <- comparison_total %>% 
+  filter(old_a_b %in% dup_links_total$Var1) %>% 
+  mutate(gap=abs(volume-count))
+comparison_total_dup2 <- comparison_total_dup %>% 
+  group_by(old_a_b) %>% 
+  filter(gap == min(gap))
+comparison_total_dup2$gap <- NULL
+comparison_total1 <- comparison_total %>% 
+  filter(!(old_a_b %in% comparison_total_dup2$old_a_b))
+comparison_total <- merge(comparison_total1, comparison_total_dup2, all=T)
+
+comparison <- merge(comparison_EA, merge(comparison_AM, merge(comparison_MD, merge(comparison_PM, merge(comparison_EV, comparison_total, all=T), all=T), all=T), all=T), all=T)
 
 # write final output
-write.csv(comparison, file.path(volOutput, "volume_vs_count.csv"), row.names = F)
-
-
-
-
-
-
-
-
+write.csv(comparison, paste(OutputDir, "volume_vs_count.csv", sep = "\\"), row.names = F)
 
 ## finish
